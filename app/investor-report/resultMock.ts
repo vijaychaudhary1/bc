bce2a6012caa2ad4541a1fc6442d79491736417c
Vijay Chaudhary <40912649+vijaychaudhary1@users.noreply.github.com>
2018-08-19 10:24:22 -0400

    Add files via upload

---
+++ app/investor-report/resultMock.ts
@@ -0,0 +1,87 @@
+export const MOCKDATA = [
+    {
+        "investorName": "Bank of Coimbatore",
+        "program": "Coromandel Train",
+        "limit": "100000",
+        "ccySold": "USD",
+        "investorPricing": "60bps",
+        "supplierExcluded": "ABC Ltd",
+        "supplierCountriesPermitted": "Europe",
+        "minTenor": "15",
+        "maxTenor": "106"
+    },
+    {
+        "investorName": "Bank of Madras",
+        "program": "Ocean Candies",
+        "limit": "50000",
+        "ccySold": "USD",
+        "investorPricing": "80bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "9",
+        "maxTenor": "90"
+    },
+    {
+        "investorName": "Bank of Madras1",
+        "program": "Ocean Candies1",
+        "limit": "70000",
+        "ccySold": "USD",
+        "investorPricing": "50bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "8",
+        "maxTenor": "80"
+    },
+    {
+        "investorName": "Bank of Madras1",
+        "program": "Ocean Candies1",
+        "limit": "70000",
+        "ccySold": "USD",
+        "investorPricing": "50bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "8",
+        "maxTenor": "80"
+    },
+    {
+        "investorName": "Bank of Madras1",
+        "program": "Ocean Candies1",
+        "limit": "70000",
+        "ccySold": "USD",
+        "investorPricing": "50bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "8",
+        "maxTenor": "80"
+    },{
+        "investorName": "Bank of Madras1",
+        "program": "Ocean Candies1",
+        "limit": "70000",
+        "ccySold": "USD",
+        "investorPricing": "50bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "8",
+        "maxTenor": "80"
+    },{
+        "investorName": "Bank of Madras1",
+        "program": "Ocean Candies1",
+        "limit": "70000",
+        "ccySold": "USD",
+        "investorPricing": "50bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "8",
+        "maxTenor": "80"
+    },{
+        "investorName": "Bank of Madras1",
+        "program": "Ocean Candies1",
+        "limit": "70000",
+        "ccySold": "USD",
+        "investorPricing": "50bps",
+        "supplierExcluded": "XYZ Ltd",
+        "supplierCountriesPermitted": "US",
+        "minTenor": "8",
+        "maxTenor": "80"
+    }
+];
