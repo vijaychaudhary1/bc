fe1974f8b472c95ccc48051848d5790cfe25b735
Vijay Chaudhary <40912649+vijaychaudhary1@users.noreply.github.com>
2018-08-20 11:25:11 -0400

    Add files via upload

---
+++ investor-report/resultMock.ts
@@ -0,0 +1,40 @@
+export const MOCKDATA = 
+[
+    {
+ "investorName": "Bank of Coimbatore ",
+ "program": "Coromandel Train Company",
+ "limit": "USD 100 mn",
+ "ccySold": "EUR, USD",
+ "investorPricing": "60 bps",
+ "supplierExcluded": "ABC Ltd, Xyz Ltd",
+ "supplierCountiesPermitted": "Europe, US",
+ "obligorCodesPermitted": "Ericsson AB, LM Ericsson & Ericsson GMBH",
+ "minTenor": 15,
+ "maxTenor": 200,
+ "minBookingMargin": 5,
+ "maximumNumberOfNotesAllowed": 5,
+ "minimumNoteSize": 100000,
+ "lastUpdated": "2018-08-19T17:12:35.765+0000",
+ "toDate": "2018-08-19T17:12:35.765+0000",
+ "fromDate": "2018-08-19T17:12:35.766+0000"
+},
+    {
+ "investorName": "Bank of Coimbatore ",
+ "program": "Coromandel Train Company",
+ "limit": "USD 100 mn",
+ "ccySold": "EUR, USD",
+ "investorPricing": "60 bps",
+ "supplierExcluded": "ABC Ltd, Xyz Ltd",
+ "supplierCountiesPermitted": "Europe, US",
+ "obligorCodesPermitted": "Ericsson AB, LM Ericsson & Ericsson GMBH",
+ "minTenor": 15,
+ "maxTenor": 200,
+ "minBookingMargin": 5,
+ "maximumNumberOfNotesAllowed": 5,
+ "minimumNoteSize": 100000,
+ "lastUpdated": "2018-08-19T17:21:37.545+0000",
+ "toDate": "2018-08-19T17:21:37.546+0000",
+ "fromDate": "2018-08-19T17:21:37.546+0000"
+}
+  
+]
